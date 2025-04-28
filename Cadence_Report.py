ee9a8b10d71e9e27026d283994ba66d4a8c9542f
BalarajBGulabal <115410443+iamkarthikfromba@users.noreply.github.com>
2025-04-28 12:35:27 +0530

    Update Cadence_Report.py

--- Cadence_Report.py
+++ Cadence_Report.py
@@ -36,7 +36,7 @@ def style_excel(df):
 
         header_font = Font(name='Calibri', size=11, bold=True)
         regular_font = Font(name='Calibri', size=11)
-        header_fill = PatternFill(start_color='D9E1F2', end_color='D9E1F2', fill_type="solid")
+        header_fill = PatternFill(start_color='BDD7EE', end_color='BDD7EE', fill_type="solid")
 
         for row in worksheet.iter_rows():
             for cell in row:
